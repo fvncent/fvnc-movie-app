2678ea817b04dcc3c107cd88fb6875c7bc0e130d
vincent <fvince12@gmail.com>
2022-07-23 12:50:03 +0800

    components and routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-// import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Movies, MovieInformation, Actors, Profile, Navbar } from './index';
 
 function App() {
   return (
     <div>
       <CssBaseline />
-      <h1>Hello Workd</h1>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" />
+          <Route path="/movie" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
